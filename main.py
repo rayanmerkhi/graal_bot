1a9b55a2d5f36694346ba2f2bd79cdd9cd7814d3
Rayan Merkhi <rayanmerkhi@yahoo.fr>
2024-03-28 17:50:16 +0100

    Amélioration des differents roll

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from random import randint
 from tokened import token_value
-
+from src.bouton_normal import ButtonsNorm
+from src.bouton_avantage import ButtonsAv
+from src.bouton_full import ButtonsFull
+from random import randint
 from dotenv import load_dotenv
 
 intents = discord.Intents.default()
@@ -27,8 +29,8 @@ async def synchronize(context):
 
     await context.send(response)
 
-@bot.tree.command(name='roll', description='Permet de jeter les dés')
-async def roll(interaction: discord.Interaction, jet: str):
+@bot.tree.command(name='jet_simple', description='Permet de jeter les dés')
+async def jet_simple(interaction: discord.Interaction, jet: str):
     x=jet.split(" ")
     tp=''
     for roll in x:
@@ -41,64 +43,26 @@ async def roll(interaction: discord.Interaction, jet: str):
     await interaction.response.send_message(tp)
 
 
-class Buttons(discord.ui.View):
-    def __init__(self, *, timeout=180):
-        super().__init__(timeout=timeout)
-        self.to_roll=[]
-    
-    @discord.ui.button(label="d4",style=discord.ButtonStyle.gray,row=0) # or .primary
-    async def dfour(self,interaction:discord.Interaction,button:discord.ui.Button):
-        button.disabled=True
-        self.to_roll.append(4)
-        await interaction.response.edit_message(view=self)
-
-    @discord.ui.button(label="d6",style=discord.ButtonStyle.gray,row=0) # or .secondary/.grey
-    async def dsix(self,interaction:discord.Interaction,button:discord.ui.Button):
-        button.disabled=True
-        self.to_roll.append(6)
-        await interaction.response.edit_message(view=self)
-    
-    @discord.ui.button(label="d8",style=discord.ButtonStyle.gray,row=0) # or .success
-    async def deight(self,interaction:discord.Interaction,button:discord.ui.Button):
-        button.disabled=True
-        self.to_roll.append(8)
-        await interaction.response.edit_message(view=self)
-    
-    @discord.ui.button(label="d10",style=discord.ButtonStyle.gray,row=1) # or .danger
-    async def dten(self,interaction:discord.Interaction,button:discord.ui.Button):
-        button.disabled=True
-        self.to_roll.append(10)
-        await interaction.response.edit_message(view=self)
-    
-    @discord.ui.button(label="d12",style=discord.ButtonStyle.gray,row=1) # or .secondary/.grey
-    async def dtwelve(self,interaction:discord.Interaction,button:discord.ui.Button):
-        button.disabled=True
-        self.to_roll.append(12)
-        await interaction.response.edit_message(view=self)
-    
-    @discord.ui.button(label="d20",style=discord.ButtonStyle.gray,row=1) # or .success
-    async def dtwenty(self,interaction:discord.Interaction,button:discord.ui.Button):
-        button.disabled=True
-        self.to_roll.append(20)
-        await interaction.response.edit_message(view=self)
-    
-    @discord.ui.button(label="submit",style=discord.ButtonStyle.green,row=2) # or .success
-    async def submit(self,interaction:discord.Interaction,button:discord.ui.Button):
-        button.disabled=True
-        tp=''
-        for roll in self.to_roll:
-            tp += 'Jet de '
-            tp +='d'+str(roll)+': '
-            tp+=str(randint(1,roll))+' '
-            tp+='\n'
-        await interaction.response.send_message(tp)
-        self.stop()
-
-@bot.tree.command(name="upgraded",description="Jet de dé avec bouttons")
-async def upgraded(interaction: discord.Interaction):
-    view=Buttons()
+@bot.tree.command(name="roll",description="Jet de dé")
+async def roll(interaction: discord.Interaction, nb_dice:str='0', value:str='1000'):
+    # ButtonsNorm est une class qui represente le design des boutons
+    # de base nb_dice a 0 car comme ca pas de blocage
+    view=ButtonsNorm(nb_dice=int(nb_dice),val=int(value))
     await interaction.response.send_message("Choisissez les dés",view=view)
 
+@bot.tree.command(name="roll_avantage",description="Jet de dé avec avantage")
+async def roll_avantage(interaction: discord.Interaction, nb_dice:str='0', value:str='1000'):
+    # ButtonsAv est une class qui represente le design des boutons
+    # de base nb_dice a 0 car comme ca pas de blocage
+    view=ButtonsAv(nb_dice=int(nb_dice),val=int(value))
+    await interaction.response.send_message("Choisissez les dés",view=view)
+
+@bot.tree.command(name="roll_avantage",description="Jet de dé avec avantage")
+async def roll_full(interaction: discord.Interaction, nb_dice:str='0', value:str='1000'):
+    # ButtonsFull est une class qui represente le design des boutons
+    # de base nb_dice a 0 car comme ca pas de blocage
+    view=ButtonsFull(nb_dice=int(nb_dice),val=int(value))
+    await interaction.response.send_message("Choisissez les dés",view=view)
 
 @bot.event
 async def on_ready():
---
+++ src/bouton_avantage.py
@@ -0,0 +1,121 @@
+import discord
+
+from random import randint
+
+class ButtonsAv(discord.ui.View):
+    def __init__(self, *, timeout=180,nb_dice=3,val=30):
+        super().__init__(timeout=timeout)
+        self.to_roll=[]
+        self.rest=nb_dice
+        self.val=val
+        self.retry=True
+    
+    @discord.ui.button(label="d4",style=discord.ButtonStyle.gray,row=0) # or .primary
+    async def dfour(self,interaction:discord.Interaction,button:discord.ui.Button):
+        if 4 in self.to_roll:
+            retry=False
+        if not self.retry:
+            button.disabled=True
+        self.rest-=1
+        if self.rest ==0:
+            for child in self.children:
+                if child.row!=2:
+                    child.disabled=True
+
+        self.to_roll.append(4)
+        await interaction.response.edit_message(view=self)
+
+    @discord.ui.button(label="d6",style=discord.ButtonStyle.gray,row=0) # or .secondary/.grey
+    async def dsix(self,interaction:discord.Interaction,button:discord.ui.Button):
+        if 6 in self.to_roll:
+            retry=False
+        if not self.retry:
+            button.disabled=True
+        self.rest-=1
+        if self.rest ==0:
+            for child in self.children:
+                if child.row!=2:
+                    child.disabled=True
+        self.to_roll.append(6)
+        await interaction.response.edit_message(view=self)
+    
+    @discord.ui.button(label="d8",style=discord.ButtonStyle.gray,row=0) # or .success
+    async def deight(self,interaction:discord.Interaction,button:discord.ui.Button):
+        if 8 in self.to_roll:
+            retry=False
+        if not self.retry:
+            button.disabled=True
+        self.to_roll.append(8)
+        self.rest-=1
+        if self.rest ==0:
+            for child in self.children:
+                if child.row!=2:
+                    child.disabled=True
+        await interaction.response.edit_message(view=self)
+    
+    @discord.ui.button(label="d10",style=discord.ButtonStyle.gray,row=1) # or .danger
+    async def dten(self,interaction:discord.Interaction,button:discord.ui.Button):
+        if 10 in self.to_roll:
+            retry=False
+        if not self.retry:
+            button.disabled=True
+        self.to_roll.append(10)
+        self.rest-=1
+        if self.rest ==0:
+            for child in self.children:
+                if child.row!=2:
+                    child.disabled=True
+        await interaction.response.edit_message(view=self)
+    
+    @discord.ui.button(label="d12",style=discord.ButtonStyle.gray,row=1) # or .secondary/.grey
+    async def dtwelve(self,interaction:discord.Interaction,button:discord.ui.Button):
+        if 12 in self.to_roll:
+            retry=False
+        if not self.retry:
+            button.disabled=True
+        self.to_roll.append(12)
+        self.rest-=1
+        if self.rest ==0:
+            for child in self.children:
+                if child.row!=2:
+                    child.disabled=True
+
+        await interaction.response.edit_message(view=self)
+    
+    @discord.ui.button(label="d20",style=discord.ButtonStyle.gray,row=1) # or .success
+    async def dtwenty(self,interaction:discord.Interaction,button:discord.ui.Button):
+        if 20 in self.to_roll:
+            retry=False
+        if not self.retry:
+            button.disabled=True
+        self.to_roll.append(20)
+        self.rest-=1
+        if self.rest ==0:
+            for child in self.children:
+                if child.row!=2:
+                    child.disabled=True
+        await interaction.response.edit_message(view=self)
+    
+    @discord.ui.button(label="submit",style=discord.ButtonStyle.green,row=2) # or .success
+    async def submit(self,interaction:discord.Interaction,button:discord.ui.Button):
+        tp=''
+        crit = True
+        sum=0
+        for roll in self.to_roll:
+            tp += 'Jet de '
+            tp +='d'+str(roll)+': '
+            nb=randint(1,roll)
+            sum+=nb
+            tp+=str(nb)+' '
+            tp+='\n'
+            if roll != nb:
+                crit =False
+        tp+='Somme: '+str(sum)+'\n'
+        if crit:
+            tp+='Succès Critique !\n'
+        elif abs(self.val-sum)<15 & self.val!=1000:
+            tp+='Echec Critique !\n'
+        await interaction.response.send_message(tp)
+        for child in self.children:
+            child.disabled=True
+        self.stop()
